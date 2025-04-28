2afba35f23ad54dbb704d902fed7116315dd2dd9
Matteo Cavestri <matteo.cavestri@protonmail.ch>
2025-04-28 14:18:36 +0200

    added meson

---
+++ main/meson/template.py
@@ -0,0 +1,30 @@
+pkgname = "meson"
+pkgver = "1.7.2"
+pkgrel = 1
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-devel",
+    "python-installer",
+    "python-setuptools",
+]
+depends = ["python", "ninja"]
+pkgdesc = "Meson build system"
+license = "Apache-2.0"
+url = "https://mesonbuild.com"
+source = f"https://github.com/mesonbuild/meson/releases/download/{pkgver}/meson-{pkgver}.tar.gz"
+sha256 = "4d40d63aa748a9c139cc41ab9bffe43edd113c5639d78bde81544ca955aea890"
+# meson is early in our bootstrap path but has a million checkdepends
+options = ["!check"]
+
+
+def post_install(self):
+    # vim syntax
+    self.install_dir("usr/share/vim/vimfiles")
+    for f in ["ftdetect", "ftplugin", "indent", "syntax"]:
+        self.install_files(
+            f"data/syntax-highlighting/vim/{f}", "usr/share/vim/vimfiles"
+        )
+    with self.pushd("data/shell-completions"):
+        self.install_completion("bash/meson", "bash")
+        self.install_completion("zsh/_meson", "zsh")
