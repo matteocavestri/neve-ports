4d106584abde98dd8a9e4386bd22685c56ee7f08
Matteo Cavestri <matteo.cavestri@protonmail.ch>
2025-05-04 17:41:52 +0200

    added ninja:1.12.1

---
+++ main/ninja/template.py
@@ -0,0 +1,31 @@
+pkgname = "ninja"
+pkgver = "1.12.1"
+pkgrel = 1
+hostmakedepends = ["python"]
+pkgdesc = "Small build system with a focus on speed"
+license = "Apache-2.0"
+url = "https://ninja-build.org"
+source = f"https://github.com/ninja-build/ninja/archive/v{pkgver}.tar.gz"
+sha256 = "821bdff48a3f683bc4bb3b6f0b5fe7b2d647cf65d52aeb63328c91a6c6df285a"
+# Cycle: ninja -> gtest -> ninja
+options = ["!check"]
+
+
+def configure(self):
+    self.do("python", "configure.py", "--bootstrap")
+
+
+def build(self):
+    self.do("python", "configure.py")
+
+
+def install(self):
+    self.install_bin("ninja")
+    self.install_file(
+        "misc/bash-completion",
+        "usr/share/bash-completion/completions",
+        name="ninja",
+    )
+    self.install_file(
+        "misc/zsh-completion", "usr/share/zsh/site-functions", name="_ninja"
+    )
