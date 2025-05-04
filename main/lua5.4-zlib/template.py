8f1be7e87f8a4250bfcb90da29857688234b6221
Matteo Cavestri <matteo.cavestri@protonmail.ch>
2025-05-04 16:45:43 +0200

    added lua:5.4

---
+++ main/lua5.4-zlib/template.py
@@ -0,0 +1,20 @@
+pkgname = "lua5.4-zlib"
+pkgver = "1.2"
+pkgrel = 1
+build_style = "makefile"
+make_build_target = "linux"
+make_build_args = ["LUA_VER=5.4"]
+hostmakedepends = ["pkgconf"]
+makedepends = ["lua5.4-devel", "zlib-ng-compat-devel"]
+pkgdesc = "Zlib streaming interface for Lua 5.4"
+license = "MIT"
+url = "https://github.com/brimworks/lua-zlib"
+source = f"{url}/archive/v{pkgver}.tar.gz"
+sha256 = "26b813ad39c94fc930b168c3418e2e746af3b2e80b92f94f306f6f954cc31e7d"
+# no test suite
+options = ["!check"]
+
+
+def install(self):
+    self.install_license("README")
+    self.install_file("zlib.so", "usr/lib/lua/5.4", mode=0o755)
