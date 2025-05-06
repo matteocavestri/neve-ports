ce6e8e1ee410ff2bb83de2e5e9b6b2d7eeb85b72
Matteo Cavestri <matteo.cavestri@protonmail.ch>
2025-05-06 15:19:59 +0200

    added nghttp2:1.65.0

---
+++ main/nghttp2/template.py
@@ -0,0 +1,37 @@
+pkgname = "nghttp2"
+pkgver = "1.65.0"
+pkgrel = 1
+build_style = "gnu_configure"
+configure_gen = []
+hostmakedepends = ["pkgconf"]
+makedepends = [
+    "c-ares-devel",
+    "jansson-devel",
+    "libev-devel",
+    "libevent-devel",
+    "libxml2-devel",
+    "openssl3-devel",
+    "zlib-ng-compat-devel",
+]
+checkdepends = ["cppunit-devel"]
+pkgdesc = "HTTP/2 C Library"
+license = "MIT"
+url = "https://nghttp2.org"
+source = f"https://github.com/tatsuhiro-t/nghttp2/releases/download/v{pkgver}/nghttp2-{pkgver}.tar.xz"
+sha256 = "f1b9df5f02e9942b31247e3d415483553bc4ac501c87aa39340b6d19c92a9331"
+# CFI; reproduces in e.g. libsoup
+hardening = ["vis", "!cfi"]
+
+
+def post_install(self):
+    self.install_license("COPYING")
+
+
+@subpackage("nghttp2-devel")
+def _(self):
+    return self.default_devel()
+
+
+@subpackage("nghttp2-progs")
+def _(self):
+    return self.default_progs(extra=["usr/share/nghttp2/fetch-ocsp-response"])
